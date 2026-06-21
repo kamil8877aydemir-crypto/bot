fab46265452a91d247e5a2fbd600a30ff1c09ebd
kamil8877aydemir-crypto <kamil8877aydemir@gmail.com>
2026-06-21 05:39:09 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import sqlite3
 from telegram import Update, ChatMember
 from telegram.ext import Application, CommandHandler, MessageHandler, ContextTypes, filters
 
-TOKEN = "8695660286:AAFfwO77m7pa7-hgiZe57NmMkz2DbsJ-BDY"
+TOKEN = ""
 DB_PATH = "bot_data.db"
 CATEGORIES = ["pos", "saha"]
 
